33c5aa45907cc9548967163744ec1c80c09e9c71
HesseyInsight <leeyoungjeon0511@gmail.com>
2020-04-16 17:12:06 +0900

    :bookmark: need to use GPU for Learning

---
+++ .ipynb_checkpoints/test_pkl-checkpoint.py
@@ -0,0 +1,11 @@
+import pickle
+
+data = {
+    'a' : [1, 2.0, 3, 4+6j],
+    'b' : ("character string", b"byte string"),
+    'c' : {None, True, False}
+}
+
+# save
+with open('data.pickle', 'wb') as f:
+    pickle.dump(data, f)
