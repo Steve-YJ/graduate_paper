:bug: change last layer to softmax!!

03. Classifier/LeNet.py
@@ -27,7 +27,7 @@ class Net(nn.Module):
         x = F.relu(self.fc1(x))
         x = F.relu(self.fc2(x))
         x = self.fc3(x)
-        return x
+        return F.log_softmax(x)  # 마지막 return값은 log_softmax()  
 
     def num_features(self, x):
         size = x.size()[1:]